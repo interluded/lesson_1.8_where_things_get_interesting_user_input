248af355fdc79cc34a01dab76174caa58e3efa5f
marclmao <marcuslmao1@gmail.com>
2023-09-21 14:04:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+                System.out.print("How old is ballz: ");
+        int age = scan.nextInt();
+        System.out.print("ballz is ");
+        System.out.println(age);
+    }
+}
