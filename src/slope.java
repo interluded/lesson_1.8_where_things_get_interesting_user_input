f94721ef8b9181984454828d22ffd47031b4a640
marclmao <119444616+marclmao@users.noreply.github.com>
2023-09-29 14:10:50 -0400

    added slope calc

--- src/slope.java
+++ src/slope.java
@@ -1,1 +1,20 @@
-//TO BE DONE
+import java.util.Scanner;
+public class slope
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the x1 double ");
+        double integer_1 = scan.nextDouble();
+        System.out.print("Enter the x2 double ");
+        double integer_2 = scan.   nextDouble();
+        System.out.print("Enter the y1 double ");
+        double integer_3 = scan.nextDouble();
+        System.out.print("Enter y2 double ");
+        double integer_4 = scan.nextDouble();
+        System.out.println((integer_2 - integer_1) / (integer_4 - integer_3));
+
+
+
+    }
+}
